b0bf2ff7e7cc33c1140b976f26a824e5830e0c2e
LutfianaDeka <lutfianadeka00@gmail.com>
2023-03-20 20:04:56 +0700

    Tugas_Class_Object_Taxi

---
+++ Tugas_Class_Object_Taxi/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Tugas_Class_Object_Taxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Membuat objek taxi
+            Taxi taxi = new Taxi();
+
+            // Pengesetan nilai properties
+            taxi.DriverName = "Jono";
+            taxi.Onduty = true;
+            taxi.NumPassanger = 10;
+
+            // Panggilan method
+            taxi.TaxiInfo();
+
+            Console.WriteLine();
+
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Tugas_Class_Object_Taxi/Taxi.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tugas_Class_Object_Taxi
+{
+    public class Taxi
+    {
+        //Properties
+        public string DriverName { get; set; }
+        public Boolean Onduty { get; set; }
+        public int NumPassanger { get; set; }
+
+        //Method
+       public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name : {0}", DriverName);
+            if (Onduty == true)
+            {
+                Console.WriteLine("On Duty : Yes");
+            }
+            else if (Onduty == false)
+            {
+                Console.WriteLine("On Duty : No");
+            }
+            Console.WriteLine("Num Passangger : {0}", NumPassanger);
+        }
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("{0} sedang menjemput penumpang", DriverName);
+        }
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("{0} selesai mengantar penumpang", DriverName);
+        }
+    }
+}
